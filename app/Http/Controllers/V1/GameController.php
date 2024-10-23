16caaef9d9ddd60ad84d503609a557e525c4cc85
Lance <ronquillolance@gmail.com>
2024-10-24 01:35:07 +0800

    Setup for IGDB API Package

---
+++ app/Http/Controllers/V1/GameController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
